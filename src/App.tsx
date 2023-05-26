18e8889b1ed5383695c43b3a82529b5d0884fd99
Davidmide Adenusi <Davidmide07@gmail.com>
2023-05-26 14:56:23 +0100

    functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import Header from "./header";
 import Orders from "./orders";
 
 function App() {
+
   return (
     <>
       <div className="">
         {/* <h1>Hello Ecommerce</h1> */}
         <Header />
-        <main className="border-2 border-gray-500 mt-5">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        <main className="border-2 border-gray-500 mt-5 md:flex justify-center p-4">
+          <div className="">
+            <Card productName="Nike watch" color="white" />
+            <Card productName="Puma watch" color="green" />
+            <Card productName="Conu watch" color="lilac" />
+            <Card productName="Apir watch" color="yellow" />
+            <Card productName="Apple watch" color="black" />
+          </div>
 
           <Orders />
         </main>
