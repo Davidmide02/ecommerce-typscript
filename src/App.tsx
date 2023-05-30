3a9ff1cb8c00191ac5de767c635424d1d7db82e8
Davidmide Adenusi <Davidmide07@gmail.com>
2023-05-30 19:34:48 +0100

    complete UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,91 @@
-import { useState } from "react";
+import React, { ReactElement, useState } from "react";
 import "./App.css";
 import Card from "./card";
 import Header from "./header";
 import Orders from "./orders";
+import shoe from "./assets/product1.jpg";
+import shoe2 from "./assets/product2.jpg";
+import shoe3 from "./assets/product3.jpg";
+import shoe4 from "./assets/product4.jpg";
+
+export type dbProps = {
+  productName: string;
+  color: string;
+  imgSrc: string;
+  amount: number;
+  // card?: React.ReactElement;
+};
+const db: dbProps[] = [
+  {
+    productName: "bip",
+    color: "white",
+    imgSrc: shoe,
+    amount: 200,
+  },
+  {
+    productName: "bid",
+    color: "white",
+    imgSrc: shoe2,
+    amount: 300,
+  },
+  {
+    productName: "bic",
+    color: "white",
+    imgSrc: shoe3,
+    amount: 200,
+  },
+  {
+    productName: "biv",
+    color: "white",
+    imgSrc: shoe4,
+    amount: 500,
+  },
+  // {
+  //   productName: "bix",
+  //   color: "white",
+  //   imgSrc: "./image.come",
+  // },
+  // {
+  //   productName: "biy",
+  //   color: "white",
+  //   imgSrc: "./src/assets/fabian-heimann-4R_WEmhx8og-unsplash.jpg",
+  // },
+];
 
 function App() {
-  let [total, setTotal] = useState(0);
+  // let [total, setTotal] = useState(0);
+  // const handleTotal = (value: number) => {
+  //   const amountn = total + value;
+  //   setTotal(value);
+  //   // console.log(amountn + " amount");
+  //   console.log(total + " total");
+  //   // console.log("hello here");
+  // };
+
+  // const totalAmount = db.reduce
+  // db.reduce ((ack=0, {amount}) => { a})
 
   return (
-    <>
-      <div className="">
-        {/* <h1>Hello Ecommerce</h1> */}
-        <Header />
-        <main className="border-2 border-gray-500 mt-5 md:flex justify-center p-4">
-          <div className="">
-            <Card productName="Nike watch" color="white" setTotal={setTotal} />
-            <Card productName="Puma watch" color="green" setTotal={setTotal} />
-            <Card productName="Conu watch" color="lilac" setTotal={setTotal} />
-            <Card productName="Apir watch" color="yellow" setTotal={setTotal} />
-            <Card productName="Apple watch" color="black" setTotal={setTotal} />
-          </div>
+    <div className="p-4">
+      <Header />
+      <main className="border-2 border-gray-500 mt-5 md:flex justify-between gap p-4 w-full">
+        <div className="card-con">
+          {db.map((item) => {
+            return (
+              <Card
+                productName={item.productName}
+                color={item.color}
+                imgSrc={item.imgSrc}
+                amount={item.amount}
+              />
+            );
+          })}
+        </div>
 
-          <Orders total={total} />
-          <h1>{total}</h1>
-        </main>
-      </div>
-    </>
+        <Orders total={4} />
+        {/* <h1>hello</h1> */}
+      </main>
+    </div>
   );
 }
 
--- src/orders.tsx
+++ src/orders.tsx
@@ -1,20 +1,23 @@
 import { GrCart } from "react-icons/gr";
 
 type orderProps = {
-
   // setTotal:React.Dispatch<React.SetStateAction<number>>;
-  total:number;
-}
+  total: number;
+};
 
-function Orders({total}:orderProps) {
+function Orders({ total }: orderProps) {
   return (
-    <div className="con w-[30%] bg-blue-500 rounded-xl flex flex-col gap-2 p-8 text-white h-[50%]">
+    <div className="con md:w-[30%] bg-blue-500 rounded-xl flex flex-col gap-2 p-8 text-white h-[50%]">
       <h2 className=" text-xl py-4">Order Sumary</h2>
-      <p className="py-4 border-b-2 border-green-500">Subtotal: NGN 0.00{total}</p>
-      <p className="py-4 border-b-2 border-green-500">Shipping extimate: NGN 0.00</p>
+      <p className="py-4 border-b-2 border-green-500">
+        Subtotal: NGN 0.00{total}
+      </p>
+      <p className="py-4 border-b-2 border-green-500">
+        Shipping extimate: NGN 0.00
+      </p>
       <p className="py-4 border-b-2 border-green-500">Tax extimate: NGN 0.00</p>
       <h4 className=" text-2xl font-bold">Total:</h4>
-      <button className="bg-blue-300 rounded-lg flex justify-center items-center gap-2 py-2 px-4 text-lg font-bold">
+      <button className="bg-white text-black rounded-lg flex justify-center items-center gap-2 py-2 px-4 text-lg font-bold">
         Checkout <GrCart />
       </button>
     </div>
